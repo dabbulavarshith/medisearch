cc7d39f86117be9a0a94c92f34f87c8df3183f9f
dabbulavarshith <dabbulavarshith@gmail.com>
2025-08-17 22:09:59 +0530

    Create precedures.js

---
+++ styles/data/precedures.js
@@ -0,0 +1,24 @@
+export const procedures = [
+  {
+    name: "Heart Bypass Surgery",
+    hospitals: [
+      { name: "Apollo, Delhi", price: "₹2,50,000" },
+      { name: "Fortis, Mumbai", price: "₹2,80,000" },
+      { name: "AIIMS, Delhi", price: "₹1,30,000" }
+    ]
+  },
+  {
+    name: "Knee Replacement",
+    hospitals: [
+      { name: "Manipal, Bangalore", price: "₹2,00,000" },
+      { name: "Fortis, Delhi", price: "₹2,40,000" }
+    ]
+  },
+  {
+    name: "Hair Transplant",
+    hospitals: [
+      { name: "Kaya, Mumbai", price: "₹80,000" },
+      { name: "Dr. Batra’s, Pune", price: "₹60,000" }
+    ]
+  }
+];
